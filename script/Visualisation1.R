901821a41a8d3d6e6d9d5bb7d86e8bb1cfb359d8
Plet <ple017@csiro.au>
2019-10-03 09:24:56 +0800

    plot with data from 1977 and log10 x scale

--- script/Visualisation1.R
+++ script/Visualisation1.R
@@ -3,3 +3,13 @@ gapminder<-read_csv("data/gapminder_data.csv")
 
 gapminder_1977<-gapminder%>%
   filter(year==1977)
+
+ggplot(gapminder_1977)
+
+ggplot(data = gapminder_1977,
+       mapping = aes(x = gdpPercap,y = lifeExp, colour = continent, size = pop)) + geom_point()
+
+ggplot(data = gapminder_1977,
+       mapping = aes(x=gdpPercap, y = lifeExp, colour = continent, size = pop)) + geom_point() + scale_x_log10()
+       
+       
