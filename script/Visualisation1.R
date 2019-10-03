e136b9e1ad20cbb607884ffba1c401910d023497
Plet <ple017@csiro.au>
2019-10-03 11:45:24 +0800

    plot of lifeExp evolution through time by continent

--- script/Visualisation1.R
+++ script/Visualisation1.R
@@ -39,6 +39,47 @@ ggplot(data = gapminder_1977) +
 #geom_point: aesthetics: x, y, alpha, colour, fill, group, shape, size, stroke
 
 
+#challenge 6: try setting soe aesthetics
+ggplot(data = gapminder_1977, 
+  mapping = aes(x = gdpPercap, 
+                y = lifeExp, 
+                colour = continent, 
+                size = pop)) +
+  geom_point(aes(alpha= pop), shape="star", colour= "pink", size=2) +
+  scale_x_log10()
+
+
+#challenge 7:  shows how life expectancy has changed over time with gapminder data
+gapminder_grouped<-gapminder%>%
+  group_by(year)%>%
+  select(year, continent, lifeExp, gdpPercap)%>%
+  summarise(mean_gdpPercap=mean (gdpPercap), mean_lifeExp=mean(lifeExp))
+  
+ggplot(gapminder,
+       aes(x=year,
+           y=lifeExp, 
+           colour= continent,
+           size=pop))+geom_point(shape= "triangle")
+
+#this one works
+ggplot(data= gapminder_grouped, 
+       mapping= aes(x=year,
+                    y=mean_lifeExp))+
+  geom_point(shape = "triangle")+
+  scale_x_continuous()
+
+#try to get mean_lifeExp per continent per year plotted
+gapminder_byContinent<-gapminder%>%
+  group_by(continent,year)%>%
+  summarise(mean_lifeExp=mean(lifeExp))
+
+
+ggplot(gapminder_byContinent,
+       aes(x=year,
+           y=mean_lifeExp, 
+           colour=continent))+
+  geom_point(shape="triangle",size=3)
+
 
 
 
