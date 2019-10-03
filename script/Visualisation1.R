cfab4114c33b63f3562e60a1cc6b842a989bbd9b
Plet <ple017@csiro.au>
2019-10-03 10:04:45 +0800

    trying different plots

--- script/Visualisation1.R
+++ script/Visualisation1.R
@@ -10,6 +10,38 @@ ggplot(data = gapminder_1977,
        mapping = aes(x = gdpPercap,y = lifeExp, colour = continent, size = pop)) + geom_point()
 
 ggplot(data = gapminder_1977,
-       mapping = aes(x=gdpPercap, y = lifeExp, colour = continent, size = pop)) + geom_point() + scale_x_log10()
+       mapping = aes(x=gdpPercap,
+                     y = lifeExp,
+                     colour = continent,
+                     size = pop)) + 
+  geom_point() + 
+  scale_x_log10()
        
-       
+
+#challenge 4
+#there are six variable in gapminder_1977, but "year" not informative
+#use default template
+
+ggplot(gapminder_1977, 
+       aes(x = country,
+           y= gdpPercap, 
+           colour= continent, 
+           size = lifeExp))+ geom_point()
+
+ggplot(data = gapminder_1977) + 
+  geom_point(mapping = aes(x=gdpPercap,
+                           y = lifeExp,
+                           colour = continent,
+                           size = pop)) + 
+  scale_x_log10()
+
+#challenge 5
+#geom_point: aesthetics: x, y, alpha, colour, fill, group, shape, size, stroke
+
+
+
+
+
+
+
+
